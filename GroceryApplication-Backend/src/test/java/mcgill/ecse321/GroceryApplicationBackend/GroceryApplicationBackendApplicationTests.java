9d056f18f07e25beec3913fd3593ec0e8519043d
Yazdan Zinati <pm@yazdan.me>
2022-02-16 09:47:50 -0500

    Add project base spring boot template

---
+++ GroceryApplication-Backend/src/test/java/mcgill/ecse321/GroceryApplicationBackend/GroceryApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mcgill.ecse321.GroceryApplicationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
