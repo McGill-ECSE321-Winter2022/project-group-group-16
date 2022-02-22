5c07bcc0d0b7d6079982aa2beea31166d8ef19a9
miki <michelle.lee6@mail.mcgill.ca>
2022-02-22 14:32:47 -0500

    Comments and spacing fixes
    Co-Authored-By: Yoah Qiu <70981597+yoahqiu@users.noreply.github.com>

--- GroceryApplication-Backend/src/test/java/ca/mcgill/ecse321/GroceryApplicationBackend/dao/TestGroceryApplicationPersistence.java
+++ GroceryApplication-Backend/src/test/java/ca/mcgill/ecse321/GroceryApplicationBackend/dao/TestGroceryApplicationPersistence.java
@@ -60,13 +60,17 @@ public class TestGroceryApplicationPersistence {
     @Autowired    
     CustomerRepository customerRepository;
     
-    
-
     @AfterEach
    public void clearDatabase() {
+<<<<<<< Updated upstream
 //        // First, we clear registrations to avoid exceptions due to inconsistencies
 //        // Then we can clear the other tables
        
+=======
+       // First, we clear registrations to avoid exceptions due to inconsistencies
+       // Then we can clear the other tables
+       //customerRepository.deleteAll();
+>>>>>>> Stashed changes
        groceryOrderRepository.deleteAll();
        storeRepository.deleteAll();
        addressRepository.deleteAll();
@@ -76,6 +80,7 @@ public class TestGroceryApplicationPersistence {
        employeeRepository.deleteAll();
        paymentRepository.deleteAll();
        groceryUserRepository.deleteAll();
+<<<<<<< Updated upstream
        managerRepository.deleteAll();
        customerRepository.deleteAll();
        
@@ -84,19 +89,27 @@ public class TestGroceryApplicationPersistence {
       
 
       
+=======
+       managerRepository.deleteAll();    
+>>>>>>> Stashed changes
    }
 
+   //persistence test for store
    @Test
     public void testPersistAndLoadStore() {
+        //create a grocery store application
+        //mandatory to create for a store to exist
         GroceryStoreApplication gs = new GroceryStoreApplication();
         gs.setId(66);
         groceryStoreApplicationRepository.save(gs);
 
-         Address address = new Address();
-         int id = 10;
-         address.setId(id);
-         addressRepository.save(address);
+        //create an address for the store
+        Address address = new Address();
+        int id = 10;
+        address.setId(id);
+        addressRepository.save(address);
 
+        //create and set up the store
         String name = "Grocery";
     	Store store = new Store();
     	store.setGroceryStoreApplication(gs);
@@ -107,79 +120,88 @@ public class TestGroceryApplicationPersistence {
     	address= null;
     	store = null;
 
+        //find store from unique id
     	store = storeRepository.findStoreByName(name);
+        //test for references w address
     	address =  addressRepository.findAddressById(id);
         assertNotNull(store);
         assertNotNull(address);
         //Can't get address here 
-       // assertNotNull(store.getAddress());
-        assertEquals(name, store.getName());
-       	    
+        //assertNotNull(store.getAddress());
+        assertEquals(name, store.getName());    	    
     }
 
+    //persistence test for Category
    @Test
-   public void testPersistAndLoadCategory() {
-	   
-	   Set <Product> productSet = new HashSet();
+    public void testPersistAndLoadCategory() {
+        //create a set of products
+        Set<Product> productSet = new HashSet();
 
-       GroceryStoreApplication gs = new GroceryStoreApplication();
-       gs.setId(66);
-       groceryStoreApplicationRepository.save(gs);
-       
-      
+        //create a grocery store application
+        //mandatory to create for a category to exist
+        GroceryStoreApplication gs = new GroceryStoreApplication();
+        gs.setId(66);
+        groceryStoreApplicationRepository.save(gs);
 
-   	int id = 11;
-       // String name = "perishable";
-       // String image = "image";
-       // String desc = "desc";
-   	Category category = new Category();
-   	category.setId(id);
-       // category.setName(name);
-    category.setGroceryStoreApplication(gs);
-    category.setName("meat");
-   	categoryRepository.save(category);
-   	
-    Product  product1 = new Product();
-    product1.setGroceryStoreApplication(gs);
-    product1.setBarcode(123);
-    product1.setCategory(category);
-    productRepository.save(product1);
+        //create and set up the category
+        int id = 11;
+        Category category = new Category();
+        category.setId(id);
+        category.setGroceryStoreApplication(gs);
+        category.setName("meat");
+        categoryRepository.save(category);
+        
+        //set up a product
+        //for references between objects
+        Product  product1 = new Product();
+        product1.setGroceryStoreApplication(gs);
+        product1.setBarcode(123);
+        product1.setCategory(category);
+        productRepository.save(product1);
+        
+        //set up another product
+        Product  product2 = new Product();
+        product2.setGroceryStoreApplication(gs);
+        product2.setBarcode(1234);
+        product2.setCategory(category);
+        productRepository.save(product2);
+        
+        //add products in the set
+        productSet.add(product2);
+        productSet.add(product1);
+        category.setProduct(productSet);
+        
+        category = null;
     
-    Product  product2 = new Product();
-    product2.setGroceryStoreApplication(gs);
-    product2.setBarcode(1234);
-    product2.setCategory(category);
-    productRepository.save(product2);
-    
-    productSet.add(product2);
-    productSet.add(product1);
-    category.setProduct(productSet);
-       
-   	category = null;
-  
-
-   	category = categoryRepository.findCategoryById(id);
-   	productSet = category.getProduct();
-       assertNotNull(category);
-       assertNotNull(productSet);
-       assertEquals(id, category.getId());  
-       assertEquals("meat", category.getName());  
+        //find category throufromgh its unique id
+        category = categoryRepository.findCategoryById(id);
+        //test references of products from category
+        productSet = category.getProduct();
+        assertNotNull(category);
+        assertNotNull(productSet);
+        assertEquals(id, category.getId());  
+        assertEquals("meat", category.getName());  
      	   
    }
 
+   //persistence test for address
     @Test
     public void testPersistAndLoadAddress() {
-
+        //create a grocery store application
+        //for references
         GroceryStoreApplication groceryStoreApplication = new GroceryStoreApplication();
         groceryStoreApplication.setId(90);
         groceryStoreApplicationRepository.save(groceryStoreApplication);
         
+        //set up a store
+        //for references
         String storeName = "storeName";
         Store store = new Store();
         store.setName(storeName);
         store.setGroceryStoreApplication(groceryStoreApplication);
         storeRepository.save(store);
        
+        //set up attributes of address
         int id = 69;
         String country = "USA";
         String province = "Province";
@@ -200,6 +222,8 @@ public class TestGroceryApplicationPersistence {
         
         address = null;
         store=null;
+
+        //test references to address with store
         store = storeRepository.findStoreByName(storeName);
         address = store.getAddress();
         assertNotNull(address);
@@ -213,9 +237,22 @@ public class TestGroceryApplicationPersistence {
         assertEquals(city, address.getCity());
     }
     
+
+    //persistence test for product
     @Test
     public void testPersistAndLoadProduct() {
+        //set up attributes for a product
+        int barCode = 123;
+        String name = "apple";
+        String description = "good apple";
+        float price = 23.50f;
+        String image = "image";
+        float weight = 11f;
+        boolean refund = false;
+        float volume = 20f;
+        int quantity = 30;
         
+<<<<<<< Updated upstream
     int barCode = 123;
     String name = "apple";
     String description = "good apple";
@@ -264,20 +301,68 @@ public class TestGroceryApplicationPersistence {
 
     
    
+=======
+        //create a grocery store application
+        //mandatory to create for a store to exist
+        GroceryStoreApplication groceryStoreApplication = new GroceryStoreApplication();
+        groceryStoreApplication.setId(90);
+        groceryStoreApplicationRepository.save(groceryStoreApplication);
+        
+        //create a new category
+        //for references
+        Category category = new Category();
+        category.setGroceryStoreApplication(groceryStoreApplication);
+        category.setId(11);
+        categoryRepository.save(category);
+        
+        //create and set up the product attributes
+        Product  product = new Product();
+        product.setGroceryStoreApplication(groceryStoreApplication);
+        product.setCategory(category);
+        product.setBarcode(barCode);
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setWeight(weight);
+        product.setImage(image);
+        product.setIsRefundable(refund);
+        product.setVolume(volume);
+        product.setAvailableQuantity(quantity);
+        product.setAvailability( Availability.DELIVERY);
+        productRepository.save(product);
+        
+        product = null;
+        category = null;
+        
+        //find product with unique id
+        product = productRepository.findProductByBarcode(barCode);
+        //test references of category from product
+        category = product.getCategory();
+        assertNotNull(product);
+        assertNotNull(product.getCategory());
+        assertNotNull(category);
+        assertEquals(product.getCategory().getId(),category.getId());
+        assertEquals(product.getBarcode(),barCode);
+        assertEquals(product.getName(),name);
+>>>>>>> Stashed changes
     }
     
+    //persistence test for shift
     @Test
     public void testPersistAndLoadShift() {
-    	
+    	//create a grocery store app
     	GroceryStoreApplication gs = new GroceryStoreApplication ();
     	gs.setId(222);
     	groceryStoreApplicationRepository.save(gs);
     	
+        //create an employee
+        //mandatory to create for a shift to exist
     	Employee employee = new Employee();
     	employee.setId(123);
     	employee.setGroceryStoreApplication(gs);
     	employeeRepository.save(employee);
     	
+        //create and set up a shift
     	Shift shift = new Shift();
     	shift.setId(111);
     	shift.setDay(Shift.Day.FRIDAY);
@@ -288,31 +373,31 @@ public class TestGroceryApplicationPersistence {
     	shift = null;
     	employee = null;
     	
+        //find shift from unique id
     	shift = shiftRepository.findShiftById(111);
     	employee = shift.getEmployee();
-    	
     	assertNotNull(shift);
     	assertNotNull(employee);
     	assertNotNull(shift.getEmployee());
     	assertEquals(shift.getEmployee().getId(),123);
-    	assertEquals(shift.getDay(),Day.FRIDAY);
-    	
-    	
-      	
-    	
-    	
+    	assertEquals(shift.getDay(),Day.FRIDAY);	
     }
+
+    //persistence test for a grocery order
     @Test
     public void testPersistAndLoadGroceryOrder() {
+        //create a grocery store application
+        //mandatory to create for a store to exist
       	GroceryStoreApplication gs = new GroceryStoreApplication ();
     	gs.setId(011);
     	groceryStoreApplicationRepository.save(gs);
     	
+        //create an address
     	Address address = new Address();
     	address.setId(808);
     	addressRepository.save(address);
     	
-    	
+    	//create and set up an order
     	GroceryOrder order = new GroceryOrder();
     	order.setGroceryStoreApplication(gs);
     	order.setId(999);
@@ -323,27 +408,32 @@ public class TestGroceryApplicationPersistence {
     	order=null;
     	address=null;
     	
+        //find grocery order from unique id
     	order = groceryOrderRepository.findGroceryOrderById(999);
+        //test references of order to associated address
     	address = order.getBillingAddress();
     	assertNotNull(order);
     	assertNotNull(address);
     	assertEquals(order.getCustomerNote(),"no nuts");
     	assertEquals(order.getBillingAddress().getId(),808);
-   	
     }
     
+    //persistence test for payment
     @Test
     public void testPersistAndLoadPayment() {
-    	
+    	//set up a grocery store application
        	GroceryStoreApplication gs = new GroceryStoreApplication ();
     	gs.setId(90);
     	groceryStoreApplicationRepository.save(gs);
     	
+        //set up a grocery order
+        //mandatory to create for a payment to exist
     	GroceryOrder order = new GroceryOrder();
     	order.setGroceryStoreApplication(gs);
     	order.setId(666);
     	groceryOrderRepository.save(order);
     	
+        //create and set up a payment
     	Payment payment = new Payment();
     	payment.setId(59);
     	payment.setPaymentCode("f4x");
@@ -352,26 +442,35 @@ public class TestGroceryApplicationPersistence {
     	
     	payment = null;
     	order=null;
+
+        //find payment from unique id
     	payment = paymentRepository.findPaymentById(59);
+        //test payment to associated order
     	order =  payment.getOrder();    	
     	assertNotNull(payment);
     	assertNotNull(order);
     	assertEquals(payment.getId(),59);
-    	assertEquals(payment.getPaymentCode(),"f4x");
-    	
+    	assertEquals(payment.getPaymentCode(),"f4x");	
     }
+<<<<<<< Updated upstream
        
+=======
+    
+    //persistence test for employee
+>>>>>>> Stashed changes
     @Test
     public void testPersistAndLoadEmployee() {
-    	
+    	//create a grocery store application
     	GroceryStoreApplication gs = new GroceryStoreApplication ();
     	gs.setId(95);
     	groceryStoreApplicationRepository.save(gs);
     	
+        //create a grocery user
     	GroceryUser gu = new GroceryUser();
     	gu.setEmail("johnnysins@gmail.com");
     	groceryUserRepository.save(gu);
     	
+        //create and set up an employee
     	Employee employee = new Employee();
     	employee.setId(1234567);
     	employee.setUser(gu);
@@ -381,9 +480,10 @@ public class TestGroceryApplicationPersistence {
     	gu=null;
     	employee = null;
     	
+        //find employee from unique id
     	employee = employeeRepository.findEmployeeById(1234567);
+        //test if employee is proper user
     	gu = employee.getUser();
-    	
     	assertNotNull(employee);
     	assertNotNull(gu);
     	assertNotNull(employee.getUser());
@@ -391,36 +491,49 @@ public class TestGroceryApplicationPersistence {
     	assertEquals(employee.getUser().getEmail(),"johnnysins@gmail.com");
     }
     
+    //persistence test for grocery user
     @Test
     public void testPersistAndLoadGroceryUser() {
+<<<<<<< Updated upstream
     	
     	Set <UserRole> userSet = new HashSet();
     	
     	
+=======
+    	//create and set up a grocery user
+>>>>>>> Stashed changes
     	GroceryUser user = new GroceryUser();
     	user.setEmail("danny@gmail.com");
     	groceryUserRepository.save(user);
     	
+<<<<<<< Updated upstream
     	Employee employee = new Employee();   	
     	userSet.add(employee);
     	
+=======
+>>>>>>> Stashed changes
     	user = null;
+
+        //find user from unique id
     	user = groceryUserRepository.findGroceryUserByEmail("danny@gmail.com");
-    	
     	assertNotNull(user);
-    	assertEquals("danny@gmail.com", user.getEmail());
-    	
-    	
+    	assertEquals("danny@gmail.com", user.getEmail());    	
     }
     
+    //persistence test for manager
     @Test
     public void testPersistAndLoadManager() {
-    	
+
+    	//create a grocery store application
     	GroceryStoreApplication gs = new GroceryStoreApplication ();
     	gs.setId(110);
     	groceryStoreApplicationRepository.save(gs);
 
+<<<<<<< Updated upstream
     	
+=======
+        //create and set up a manager
+>>>>>>> Stashed changes
     	Manager manager = new Manager();
     	manager.setId(12345);
     	manager.setGroceryStoreApplication(gs);
@@ -428,35 +541,37 @@ public class TestGroceryApplicationPersistence {
     	
     	manager = null;
     	
+        //find manager from unique id
     	manager = managerRepository.findManagerById(12345);
-    	
     	assertNotNull(manager);
     	assertEquals(12345,manager.getId());
-
     }
+<<<<<<< Updated upstream
       
+=======
+    
+    //persistence test for grocery app
+>>>>>>> Stashed changes
     @Test
     public void testPersistAndLoadGroceryApplication() {
-    	
+    	//create a grocery store application
     	GroceryStoreApplication gs = new GroceryStoreApplication ();
     	gs.setId(120);
-    	
     	groceryStoreApplicationRepository.save(gs);
 	
     	gs = null;
     	
+        //find grocery store from unique id
     	gs = groceryStoreApplicationRepository.findGroceryStoreApplicationById(120);
     	assertNotNull(gs);
-    	assertEquals(120,gs.getId());
-    	
-    	
-    	
+    	assertEquals(120,gs.getId());	
     }
     
     @Test
     public void testPersistAndLoadCustomer() {
     	
     	GroceryStoreApplication gs = new GroceryStoreApplication ();
+<<<<<<< Updated upstream
         gs.setId(195);
         groceryStoreApplicationRepository.save(gs);
 
@@ -497,4 +612,24 @@ public class TestGroceryApplicationPersistence {
     
     
 
+=======
+    	gs.setId(168);
+    	groceryStoreApplicationRepository.save(gs);
+    	
+    	
+    	
+    	Customer customer = new Customer();
+    	
+    	customer.setId(789);
+    	customer.setGroceryStoreApplication(gs);
+    	customerRepository.save(customer);
+    	
+    	customer = null;
+    	
+    	customer = customerRepository.findCustomberById(789);
+    	
+    	assertNotNull(customer);
+    	assertEquals(789, customer.getId());
+    }*/
+>>>>>>> Stashed changes
 }
