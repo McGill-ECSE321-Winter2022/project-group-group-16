2b4881902c7a18cdfb113d18a3171ce026756b2e
Yazdan Zinati <pm@yazdan.me>
2022-03-26 11:35:33 -0400

    Modify config index file.

--- GroceryApplication-Frontend/config/index.js
+++ GroceryApplication-Frontend/config/index.js
@@ -8,10 +8,6 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    host: 'groceryapplication-frontend-l.herokuapp.com',
-    port: 443,
-    backendHost: 'groceryapplication-backend-l.herokuapp.com',
-    backendPort: 443,
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -23,14 +19,14 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
+    host: 'groceryapplication-frontend-l.herokuapp.com',
+    port: 443,
+    backendHost: 'groceryapplication-backend-l.herokuapp.com',
+    backendPort: 443,
   },
   dev: {
     env: require('./dev.env'),
-    host: '127.0.0.1',
-    port: 8087,
-    backendHost: '127.0.0.1',
-    backendPort: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -40,6 +36,10 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: false,
+    host: '127.0.0.1',
+    port: 8087,
+    backendHost: '127.0.0.1',
+    backendPort: 8080,
   }
 }
