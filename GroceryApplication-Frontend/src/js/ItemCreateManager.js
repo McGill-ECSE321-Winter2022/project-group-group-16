5deb37633ba79632a06f588648be1f2e102faadb
hiArturo <arturo.moryramirez@mail.mcgill.ca>
2022-04-03 20:38:15 -0400

    Made ItemCreateManager, ItemManager, and axiosinst.js files
    Co-Authored-By: NoahYe123 <90061979+NoahYe123@users.noreply.github.com>

--- GroceryApplication-Frontend/src/js/ItemCreateManager.js
+++ GroceryApplication-Frontend/src/js/ItemCreateManager.js
@@ -1,62 +1,70 @@
-import axios from 'axios'
-var config = require('../../config')
+import { AXIOS } from "./axiosinst";
+//ITEMCREATEMANAGER
+export default {
+    name: "",
 
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
-
-var AXIOS = axios.create({
-    baseURL: backendUrl,
-    headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
-
-export default{
-    name: 'ItemCreateManager',
-
-    created: function () {
-        AXIOS.get('/getAllProduct')
-            .then(response => {
-                this.products = response.data
-            })
-            .catch(e => {
-                this.errorProduct = e;
-            })
+    /**
+     * declaration of the page's data
+     */
+    data() {
+        return {
+            imageCategory: "",
+            nameCategory: "",
+            descriptionCategory: "",
+            image: "",
+            applicationId: "",
+            categoryId: "",
+            name: "",
+            description: "",
+            price: "",
+            weight: "",
+            volume: "",
+            availability: "",
+            isRefundable: "",
+            avaQuantity: "",
+        };
     },
-
-    
-    data(){
-        return{
-            image:"",
-            applicationId:"",
-            categoryId:"",
-            name:"",
-            description:"",
-            price:"",
-            weight:"",
-            volume:"",
-            availability:"",
-            isRefundable:"",
-            avaQuantity:"",
-            errorProduct:"",
-        }
-    },
-
-    products: async function () {
-        try {
-          //await this.createPayment();
-          console.log("Create product");
-          console.log(this.errorUser);
-        } catch (e) {
-          console.log(e.response)
-          if (e.response) {
-            this.errorUser = e.response.data.message
-          }
-        }
-    },
-
     methods: {
-        createProduct: async function (){
-            const res = await AXIOS.post(`/product/?image=${this.image}&name=${this.name}&pric=${this.price}&description=${this.description}&weight=${this.weight}&volume=${this.volume}&availability=${this.availability}&isRefundable=${this.isRefundable}&avaQuantity=${this.avaQuantity}`)
-        }
-    },
 
-}
+        addProduct: async function () {
+            try {
+                await this.createCategory;
+                await this.createProduct;
+            } catch (e) {
+                console.log(e.response)
+                if (e.response) {
+                    this.errorUser = e.response.data.message
+                }
+            }
+
+            // if (this.errorUser === "") {
+            //     console.log(this.email, " Product Created!")
+            //     localStorage.setItem("email", this.email)
+            //     localStorage.setItem("usertype", "customer")
+            //     this.$router.push('login')
+            // } else {
+            //     alert("Signup failed")
+            //     this.errorUser = ""
+            // }
+        },
+
+        addProduct: async function () {
+            const res = await AXIOS.post(`/category/?image=${this.imageCategory}&applicationId=0&name=${this.nameCategory}&lastName=${this.descriptionCategory}`)
+        },
+
+        createProduct: async function () {
+            // create product
+            const res = await AXIOS.put(
+                `image=${this.image}&applcationId=${this.applicationId}&
+                categoryId=${this.categoryId}&
+                name=${this.name}&
+                description=${this.description}&
+                price=${this.price}&
+                weight=${this.weight}&
+                volume=${this.volume}&
+                availability=${this.availability}&
+                isRefundable=${this.isRefundable}&
+                avaQuantity=${this.avaQuantity}`)
+        },   
+    },
+};
