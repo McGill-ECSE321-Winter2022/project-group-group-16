be3c62c4397cd9f555a34dc690b0edb5345dfa74
miki <michelle.lee6@mail.mcgill.ca>
2022-04-03 23:52:44 -0400

    Removed unnecessary lines

--- GroceryApplication-Frontend/src/js/ItemManager.js
+++ GroceryApplication-Frontend/src/js/ItemManager.js
@@ -1,53 +1,101 @@
-import axios from 'axios'
-var config = require('../../config')
+import { AXIOS } from "./axiosinst";
+//ITEMMANAGER
+export default {
+  name: "ItemManager",
 
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+  /**
+   * declaration of the page's data
+   */
+  data() {
+    return {
+        barcode: "",
+        image: "",
+        applicationId: "",
+        categoryId: "",
+        name: "",
+        description: "",
+        price: "",
+        weight: "",
+        volume: "",
+        availability: "",
+        isRefundable: "",
+        avaQuantity: "",
 
-var AXIOS = axios.create({
-    baseURL: backendUrl,
-    headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
-
-export default{
-    name: 'ItemManager',
-
-    created: function () {
-        AXIOS.get('/getAllProduct')
-            .then(response => {
-                this.products = response.data
-            })
-            .catch(e => {
-                this.errorProduct = e;
-            })
-    },
-    data(){
-        return{
-            errorProduct: '',
-            newProduct: '',
-            image:'',
-            applicationId:'',
-            categoryId:'',
-            name:'',
-            description:'',
-            price:'',
-            weight:'',
-            volume:'',
-            availability:'',
-            isRefundable:'',
-            avaQuantity:'',
+    };
+  },
+  methods: {
+    itemManagerDelete: async function () {
+        try {
+          await this.deleteProduct
+  
+        } catch (e) {
+          console.log(e.response)
+          if (e.response) {
+            this.errorUser = e.response.data.message
+          }
         }
-    },
+  
+        // if (this.errorUser === "") {
+        //   console.log(this.paymentCode, " payed!")
+        //
+        //   let data = {
+        //     order_id_param: this.orderId,
+        //     email_param: this.email
+        //   }
+        //   await this.$router.push({name: 'OrderConfirmation', params: {data}})
+        // } else {
+        //   alert("Payment failed!")
+        //   console.log(this.errorUser);
+        //   this.errorUser = ""
+        // }
+      },
 
-    methods: {
-        createProduct: function (image,applicationId,categoryId,name,description,price,weight,volume,availability,isRefundable,avaQuantity){
-            AXIOS.post('/createProduct/' + image + '/' + applicationId + '/' + categoryId + '/' + name + '/' + description + '/' + price + '/' + weight + '/' + volume + '/' + availability + '' + isRefundable + '/' + avaQuantity).then(response => {
-                this.refresh()
-                this.errorProduct=''
-            }).catch(e =>{
-                var errorMsg = e.response.data.message
-            }) 
+    itemManagerUpdate: async function () {
+      try {
+        await this.updateProduct
+
+      } catch (e) {
+        console.log(e.response)
+        if (e.response) {
+          this.errorUser = e.response.data.message
         }
+      }
+
+      // if (this.errorUser === "") {
+      //   console.log(this.paymentCode, " payed!")
+      //
+      //   let data = {
+      //     order_id_param: this.orderId,
+      //     email_param: this.email
+      //   }
+      //   await this.$router.push({name: 'OrderConfirmation', params: {data}})
+      // } else {
+      //   alert("Payment failed!")
+      //   console.log(this.errorUser);
+      //   this.errorUser = ""
+      // }
     },
 
-}
+    updateProduct: async function () {
+        //update Product
+        const res = await AXIOS.put(
+        `/product/barcode=${this.barcode}&
+        image=${this.image}&applcationId=${this.applicationId}&
+        categoryId=${this.categoryId}&
+        name=${this.name}&
+        description=${this.description}&
+        price=${this.price}&
+        weight=${this.weight}&
+        volume=${this.volume}&
+        availability=${this.availability}&
+        isRefundable=${this.isRefundable}&
+        avaQuantity=${this.avaQuantity}`)
+  
+      },
+      deleteroduct: async function () {
+        //delete Product
+        const res = await AXIOS.delete(`/product/barcode=${this.barcode}`)
+        
+      },
+    },
+  };
