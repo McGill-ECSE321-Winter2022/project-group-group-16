8c6a61ceb505ffa0dc135a036c66bafc209c55c6
miki <michelle.lee6@mail.mcgill.ca>
2022-04-04 00:02:28 -0400

    Revert "Merge branch 'main' of https://github.com/McGill-ECSE321-Winter2022/project-group-group-16"
    This reverts commit a4d07a1a34bf9b1278d16a9d8684a7af8b4f7ca9, reversing
    changes made to 87eb100716caaacd1b0df74c99940056ff45080f.

--- GroceryApplication-Frontend/src/components/Store.vue
+++ GroceryApplication-Frontend/src/components/Store.vue
@@ -1,76 +1,25 @@
 <template>
   <form>
-    <h1>Store information</h1>
+    <h1>Welcome</h1>
     <label>Store Name :</label>
     <input type="text" required />
 
-    <h5> <br /> Hours </h5>
-    <div class="row">
-      <div class="col">
-        <p> <br /> Weekdays </p>
-      </div>
-      <div class="col">
-        <label>opening hours :</label>
-        <input type="time" required />
-      </div>
-      <div class="col">
-        <label>closing hours :</label>
-        <input type="time" required />
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col">
-        <p> <br /> Weekends </p>
-      </div>
-      <div class="col">
-        <label>opening hours :</label>
-        <input type="time" required />
-      </div>
-      <div class="col">
-        <label>closing hours :</label>
-        <input type="time" required />
-      </div>
-    </div>
-
-    <h5> <br /> Address </h5>
-    <div class="row">
-      <div class="col">
-        <label>Street Number :</label>
-        <input type="number" required />
-      </div>
-      <div class="col">
-        <label>Street Name :</label>
-        <input type="text" required />
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <label>City :</label>
-        <input type="text" required v-model="city" />
-      </div>
-      <div class="col">
-       <label>Province :</label>
-        <input type="text" required v-model="province" />
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-       <label>Country :</label>
-        <input type="text" required v-model="country" />
-      </div>
-      <div class="col">
-       <label>Postal Code :</label>
-        <input type="text" required v-model="postalCode" />
-      </div>
-    </div>
-
+    <label>opening hour :</label>
+    <input type="time" required />
     <div class="button" align="center">
-      <button class="submit" type="submit"  @click="store"> Save </button>
+      <button class="submit" type="submit">Login</button>
     </div>
   </form>
 </template>
 
-<script src="../js/store.js"></script>
+<script>
+export default {
+  data() {
+    return {
+      formValues: {},
+    };
+  },
+};
+</script>
 
-<style scoped src="../css/form.css"></style>
+<style scoped src="../css/store.css"></style>
--- GroceryApplication-Frontend/src/router/index.js
+++ GroceryApplication-Frontend/src/router/index.js
@@ -4,16 +4,8 @@ import Signup from '@/components/Signup'
 import Login from '@/components/Login'
 import Four0Four from '@/components/404'
 import Store from '@/components/Store'
-import HomeScreenCustomer from '@/components/HomeScreenCustomer'
-import ItemCustomer from '@/components/ItemCustomer'
-import Employee from '@/components/Employee'
-import EmployeeProfile from '@/components/EmployeeProfile'
-import ItemManager from '@/components/ItemManager'
-import ItemCreateManager from '@/components/ItemCreateManager'
-import HomeScreenManager from '@/components/HomeScreenManager'
-import Shift from '@/components/Shift'
-import HomeScreenEmployee from '@/components/HomeScreenEmployee'
-import ShiftEmployee from '@/components/ShiftEmployee'
+import EmployeeSignup from '@/components/EmployeeSignup'
+
 
 Vue.use(Router)
 
@@ -23,28 +15,6 @@ export default new Router({
       path: '/store',
       name: 'Store',
       component: Store
-    },
-
-    {
-      path: '/cart',
-      name: 'Cart',
-      component: Cart
-    },
-
-    {
-      path: '/navbar',
-      name: 'navbar',
-      component: navbar
-    },
-    {
-      path: '/accountinfo',
-      name: 'AccountInfo',
-      component: AccountInfo
-    },
-    {
-      path: '/editingaccinfo',
-      name: 'EditingAccInfo',
-      component: EditingAccInfo
     },
     {
       path: '/employeesignup',
@@ -62,78 +32,11 @@ export default new Router({
       component: Login
     },
     {
-      path: '/homescreenCustomer',
-      name: 'HomeScreenCustomer',
-      component: HomeScreenCustomer
-    },
-    {
-      path: '/homescreenManager',
-      name: 'HomeScreenManager',
-      component: HomeScreenManager
-    },
-    {
-      path: '/homescreenEmployee',
-      name: 'HomeScreenEmployee',
-      component: HomeScreenEmployee
-    },
-    {
-      path: '/itemCustomer',
-      name: 'ItemCustomer',
-      component: ItemCustomer
-    },
-    {
-      path: '/itemManager',
-      name: 'ItemManager',
-      component: ItemManager
-    },
-    {
-      path: '/itemCreateManager',
-      name: 'ItemCreateManager',
-      component: ItemCreateManager
-    },
-    {
-      path: '/employee',
-      name: 'Employee',
-      component: Employee
-    },
-    {
-      path: '/employeeProfile',
-      name: 'EmployeeProfile',
-      component: EmployeeProfile
-    },
-    {
-      path: '/shift',
-      name: 'Shift',
-      component: Shift
-    },
-    {
-      path: '/shiftEmployee',
-      name: 'ShiftEmployee',
-      component: ShiftEmployee
-    },
-    {
       path: '/404',
       name: 'NotFound',
       component: Four0Four
 
-    },
-    {
-      path: '/order-history',
-      name: 'OrderHistory',
-      component: OrderHistory
-    },
-    {
-      path: '/order-confirmation',
-      name: 'OrderConfirmation',
-      component: OrderConfirmation,
-      meta: {auth: true}
-    },
-    {
-      path: '/payment',
-      name: 'Payment',
-      component: Payment
-    },
-    {
+    }, {
       path: '*',
       redirect: '/404'
     }
